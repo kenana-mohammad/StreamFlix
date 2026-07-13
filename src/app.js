be37c36e1965fa5c4eeb642b706b9e9fa3b5635d
Your Name <your_email@example.com>
2026-07-13 16:40:15 +0300

    complete-auth-device

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
-}))
+}));
 app.use(require('morgan')('dev'));
 const path = require("path");
 const PORT = process.env.PORT || 3000;
@@ -33,8 +34,12 @@ app.use(express.static('public'))
 app.get('/api/health', (req, res) => {
     return res.status(200).json('the api is healthy')
 });
+// auth
+app.use('/api/v1/auth', require("./modules/auth/routes/auth.routes"));
+//profile
+app.use('/api/v1/users', require("./modules/users/routes/user.routes"));
 
-
+app.use('/api/v1/devices', require("./modules/devices/routes/device.routes"));
 app.use(notFound);
 app.use(errorHandler);
 
---
+++ src/modules/auth/routes/auth.routes.js
@@ -0,0 +1,19 @@
+  const express = require("express");
+  const router = express.Router();
+  const authController = require("./../controllers/auth.controller");
+  const asyncHandler = require("./../../../utils/asyncHandler");
+  const auth = require("../../../middlewares/auth");
+  const {
+      registerValidate,
+      loginValidate,
+      changeMyPasswordValidation
+  } = require("./../validations/auth.validation");
+
+  router.post('/register', [...registerValidate], asyncHandler(authController.register))
+  router.post('/login', [...loginValidate], asyncHandler(authController.login))
+  router.post('/logout', [auth], asyncHandler(authController.logout));
+
+  router.put('/refresh-token', asyncHandler(authController.refreshToken));
+  router.put('/change-password', [auth, ...changeMyPasswordValidation], asyncHandler(authController.changeMyPassword));
+
+  module.exports = router;
