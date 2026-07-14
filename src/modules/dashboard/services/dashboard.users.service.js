6e2dca4676ecfb4594ba5367caf205be22e0f841
Yara <lilysoufi22@gmail.com>
2026-07-14 19:12:00 +0300

    minor fix

--- src/modules/dashboard/services/dashboard.users.service.js
+++ src/modules/dashboard/services/dashboard.users.service.js
@@ -11,7 +11,7 @@ class DashboardUsersService {
        const regex = new RegExp(query, 'i');
        const users = await User.find({
            $or: [
-               { username: regex },
+               { name: regex },
                { email: regex }
            ]
        })
