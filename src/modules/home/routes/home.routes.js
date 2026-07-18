b1e4adc93f81b5f58e81b11c79aa5c01c15689b2
Bushra Ani <bushraani318@gmail.com>
2026-07-18 15:21:48 +0300

    Implement Home module

---
+++ src/modules/home/routes/home.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const homeController = require("../controllers/home.controller");
+const asyncHandler = require("../../../utils/asyncHandler");
+
+const router = express.Router();
+
+router.get("/", asyncHandler(homeController.getHome));
+
+module.exports = router;
