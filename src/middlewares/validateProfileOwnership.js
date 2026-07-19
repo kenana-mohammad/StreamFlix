efc53b16223f974004ad0e92a06e12ec82b75196
Yara <lilysoufi22@gmail.com>
2026-07-19 19:45:38 +0300

    Profile CRUD

---
+++ src/middlewares/validateProfileOwnership.js
@@ -0,0 +1,32 @@
+const Profile = require('../modules/profiles/models/Profile')
+const User = require('../modules/users/models/User')
+const { errorResponse } = require("../../src/shared/helpers/api-response.helper");
+const {USER_STATUS } = require('../../src/shared/constants/user-status.constant');
+
+const validateProfileOwnership = async (req, res, next) => {
+  const id =  req.params.id;
+  const userId = req._user.id;
+  
+  const userCheck = await User.findById(userId)
+  const profile = await Profile.findOne({
+    _id: id,
+    userId: userId
+  });
+
+   if(userCheck.status != USER_STATUS.ACTIVE) {
+        return errorResponse(res, 400,
+          "User Account is not active"
+        )
+  }
+
+  if (!profile) {
+    return res.status(403).json({
+      success: false,
+      message: "Invalid profile or unauthorized",
+    });
+  }
+
+  next();
+};
+
+module.exports = validateProfileOwnership;
